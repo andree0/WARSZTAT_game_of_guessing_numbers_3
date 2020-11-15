0fff176a3f062a5346c8fb66bdfa82abcaa20d28
andrzej <jonek.andree@gmail.com>
2020-11-15 12:47:45 +0100

    flask_is_hard

--- game_3.py
+++ game_3.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def guess_of_numbers():
     if request.method == 'GET':
-        min = 0
-        max = 1000
+        user_min = 0
+        user_max = 1000
         return f'''
             <!DOCTYPE html>
             <html lang="en">
@@ -19,43 +19,20 @@ def guess_of_numbers():
             <body>
             <h1>Imagine number between 0 and 1000</h1>
             <form action="" method="POST">
-            <input type="hidden" name="min" value={min}></input>
-            <input type="hidden" name="max" value={max}></input>
-            <input type="submit" value="OK"></input>
+            <input type="hidden" name="user_min" value={user_min}>
+            <input type="hidden" name="user_max" value={user_max}>
+            <input type="submit" value="OK">
             </form>
             </body>
             </html>
             '''
     else:
         answer = input(request.form.get("answer"))
-        min = int(input(request.form.get("min")))
-        max = int(input(request.form.get("max")))
-        guess = int((max - min) / 2) + min
+        user_min = int(input(request.form.get("user_min")))
+        user_max = int(input(request.form.get("user_max")))
+        guess = int(input(request.form.get("guess")))
 
-        if min == max:
-            return f'''
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                <title>Guess The Number</title>
-                </head>
-                <body>
-                <h1>NOT CHEAT!</h1>
-                <h1>It is number {guess}</h1>
-                <form action="/" method="POST">
-                    <input type="submit" name="answer" value="Too big"></input>
-                    <input type="submit" name="answer" value="Too small"></input>
-                    <input type="submit" name="answer" value="You win"></input>
-                    <input type="hidden" name="guess" value="{guess}"></input>
-                    <input type"hidden" name="min" value={min}></input>
-                    <input type"hidden" name="max" value={max}></input>
-                </form>
-                </body>
-                </html>
-                '''
-
-        elif answer == 'You win':
+        if answer == 'You win':
             return f'''
                 <!DOCTYPE html>
                 <html lang="en">
@@ -69,10 +46,12 @@ def guess_of_numbers():
                 </html>
                 '''
         elif answer == 'Too big':
-            max = guess
+            user_max = guess
 
         elif answer == 'Too small':
-            min = guess
+            user_min = guess
+
+        guess = int((user_max - user_min) / 2) + user_min
 
         return f'''
             <!DOCTYPE html>
@@ -84,12 +63,12 @@ def guess_of_numbers():
             <body>
             <h1>It is number {guess}</h1>
                 <form action="/" method="POST">
-                    <input type="submit" name="answer" value="Too big"></input>
-                    <input type="submit" name="answer" value="Too small"></input>
-                    <input type="submit" name="answer" value="You win"></input>
-                    <input type="hidden" name="guess" value="{guess}"></input>
-                    <input type"hidden" name="min" value={min}></input>
-                    <input type"hidden" name="max" value={max}></input>
+                    <input type="submit" name="answer" value="Too big">
+                    <input type="submit" name="answer" value="Too small">
+                    <input type="submit" name="answer" value="You win">
+                    <input type="hidden" name="guess" value="{guess}">
+                    <input type"hidden" name="user_min" value={user_min}>
+                    <input type"hidden" name="user_max" value={user_max}>
                 </form>            
             </body>
             </html>
