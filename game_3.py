3b44c9bdd29c4df92cf2917a0171cc8d30ea0e83
andrzej <jonek.andree@gmail.com>
2020-11-15 01:19:09 +0100

    not_work

--- game_3.py
+++ game_3.py
@@ -7,132 +7,93 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def guess_of_numbers():
     if request.method == 'GET':
+        min = 0
+        max = 1000
         return f'''
-            <form action="/" method="POST">
-                <p>
-                    <p type"hidden" name="min" value=0>
-                    <p type"hidden" name="max" value=1000> 
-                </p>    
-            <label>
-                Pomyśl liczbę od 0 do 1000, a ja ją zgadnę w max. 10 próbkach
-            </label>
-            <label>
-                <button type="submit" name="answer" value="OK">
-                OK
-                </button>
-            </label>
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+            <title>Guess The Number</title>
+            </head>
+            <body>
+            <h1>Imagine number between 0 and 1000</h1>
+            <form action="" method="POST">
+            <input type="hidden" name="min" value={min}></input>
+            <input type="hidden" name="max" value={max}></input>
+            <input type="submit" value="OK"></input>
             </form>
+            </body>
+            </html>
             '''
-    elif request.method == 'POST':
-        answer = input(request.form["answer"])
-        min = int(input(request.form["min"]))
-        max = int(input(request.form["max"]))
+    else:
+        answer = input(request.form.get("answer"))
+        min = int(input(request.form.get("min")))
+        max = int(input(request.form.get("max")))
         guess = int((max - min) / 2) + min
 
         if min == max:
             return f'''
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                <title>Guess The Number</title>
+                </head>
+                <body>
+                <h1>NOT CHEAT!</h1>
+                <h1>It is number {guess}</h1>
                 <form action="/" method="POST">
-                <p>
-                    <p type"hidden" name="guess" value={min}></p>
-                    <p type"hidden" name="guess" value={max}> </p>
-                </p>                
-                <label>
-                    NIE OSZUKUJ!
-                </label>
-                <label>   
-                Zgaduję: {guess}
-                </label>
-                <label>   
-                    Jaka jest twoja odpowiedź
-                    <button type="submit" name="answer" value="Too big">
-                    Too big
-                    </button>
-                    <button type="submit" name="answer" value="Too small">
-                    Too small
-                    </button>
-                    <button type="submit" name="answer" value="You win">
-                    You win
-                    </button>
-                </label>
+                    <input type="submit" name="answer" value="Too big"></input>
+                    <input type="submit" name="answer" value="Too small"></input>
+                    <input type="submit" name="answer" value="You win"></input>
+                    <input type="hidden" name="guess" value="{guess}"></input>
+                    <input type"hidden" name="min" value={min}></input>
+                    <input type"hidden" name="max" value={max}></input>
                 </form>
+                </body>
+                </html>
                 '''
 
         elif answer == 'You win':
-            return "Wygrałeś!"
+            return f'''
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                <meta charset="UTF-8">
+                <title>Guess The Number</title>
+                </head>
+                <body>
+                <h1>Hurra! I guess! Your number is {guess}</h1>
+                </body>
+                </html>
+                '''
         elif answer == 'Too big':
             max = guess
-            return f'''
-                <form action="/" method="POST">
-                <p>
-                    <p type"hidden" name="guess" value={min}></p>
-                    <p type"hidden" name="guess" value={max}> </p>
-                </p>
-                <label>   
-                Zgaduję: {guess}
-                </label>                  
-                <label>
-                    Jaka jest twoja odpowiedź
-                    <button type="submit" name="answer" value="Too big">
-                    Too big
-                    </button>
-                    <button type="submit" name="answer" value="Too small">
-                    Too small
-                    </button>
-                    <button type="submit" name="answer" value="You win">
-                    You win
-                    </button>
-                </label>
-                </form>
-                '''
+
         elif answer == 'Too small':
             min = guess
-            return f'''
+
+        return f'''
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+            <meta charset="UTF-8">
+            <title>Guess The Number</title>
+            </head>
+            <body>
+            <h1>It is number {guess}</h1>
                 <form action="/" method="POST">
-                <p>
-                    <p type"hidden" name="guess" value={min}></p>
-                    <p type"hidden" name="guess" value={max}> </p>
-                </p>
-                <label>   
-                Zgaduję: {guess}
-                </label>                  
-                <label>
-                    Jaka jest twoja odpowiedź
-                    <button type="submit" name="answer" value="Too big">
-                    Too big
-                    </button>
-                    <button type="submit" name="answer" value="Too small">
-                    Too small
-                    </button>
-                    <button type="submit" name="answer" value="You win">
-                    You win
-                    </button>
-                </label>
-                </form>
-                '''
-        else:
-            return f'''
-                <form action="/" method="POST">
-                <label>   
-                Zgaduję: {guess}
-                </label>
-                <p>
-                    <p type"hidden" name="guess" value={min}></p>
-                    <p type"hidden" name="guess" value={max}> </p>
-                </p>
-                <label>
-                    Jaka jest twoja odpowiedź
-                    <button type="submit" name="answer" value="Too big">
-                    Too big
-                    </button>
-                    <button type="submit" name="answer" value="Too small">
-                    Too small
-                    </button>
-                    <button type="submit" name="answer" value="You win">
-                    You win
-                    </button>
-                </label>
-                </form>
-                '''
+                    <input type="submit" name="answer" value="Too big"></input>
+                    <input type="submit" name="answer" value="Too small"></input>
+                    <input type="submit" name="answer" value="You win"></input>
+                    <input type="hidden" name="guess" value="{guess}"></input>
+                    <input type"hidden" name="min" value={min}></input>
+                    <input type"hidden" name="max" value={max}></input>
+                </form>            
+            </body>
+            </html>
+            '''
 
 
 if __name__ == "__main__":
